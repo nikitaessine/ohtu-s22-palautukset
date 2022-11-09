a337c3c8275fe1f34bfd5c5588af023ab2194dc0
Nikita Essine <nikita.essine@helsinki.fi>
2022-11-09 17:50:29 +0200

    t12

---
+++ viikko2/verkkokauppa-1/src/pankki.py
@@ -0,0 +1,19 @@
+from kirjanpito import kirjanpito as def_kirjanpito
+
+
+class Pankki:
+
+   
+    
+    def __init__(self, kirjanpito=def_kirjanpito):
+        self._kirjanpito = kirjanpito
+
+    def tilisiirto(self, nimi, viitenumero, tililta, tilille, summa):
+        self._kirjanpito.lisaa_tapahtuma(
+            f"tilisiirto: tililtä {tililta} tilille {tilille} viite {viitenumero} summa {summa}e"
+        )
+
+        # täällä olisi koodi joka ottaa yhteyden pankin verkkorajapintaan
+        return True
+
+pankki = Pankki()
