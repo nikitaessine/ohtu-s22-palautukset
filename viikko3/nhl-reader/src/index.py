8e12fb2ad9ff40dcab4dc3bcb179b7a190514bde
Nikita Essine <nikita.essine@helsinki.fi>
2022-11-14 15:19:43 +0200

    t1

---
+++ viikko3/nhl-reader/src/index.py
@@ -0,0 +1,30 @@
+import requests
+from player import Player
+
+def main():
+    url = "https://studies.cs.helsinki.fi/nhlstats/2021-22/players"
+    response = requests.get(url).json()
+
+    print("JSON-muotoinen vastaus:")
+    print(response)
+
+    players = []
+
+    for player_dict in response:
+        player = Player(
+            player_dict['name'],
+            player_dict['team'], 
+            player_dict['goals'], 
+            player_dict['assists'],
+            player_dict['nationality']
+        )
+        if player.nationality == "FIN":
+            players.append(player)
+
+    print("Oliot:")
+
+    for player in players:
+        print(player)
+
+if __name__=="__main__":
+    main()
---
+++ viikko3/nhl-reader/src/player.py
@@ -0,0 +1,9 @@
+class Player:
+    def __init__(self, name, team, goals, assists, nationality):
+        self.name = name
+        self.team = team
+        self.goals = goals
+        self.assists = assists
+        self.nationality = nationality
+    def __str__(self):
+        return f"{self.name} team {self.team} goals {self.goals} assists {self.assists}" 
