a4aef2392ff29bfb72f92c090b7ea95273897b32
Nikita Essine <nikita.essine@helsinki.fi>
2022-11-22 15:12:19 +0200

    testataan että korissa on oikea määrä tavaroita

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -19,3 +19,10 @@ class TestOstoskori(unittest.TestCase):
         maito = Tuote('Maito', 3)
         self.kori.lisaa_tuote(maito)
         self.assertEqual(self.kori.hinta(), 3)
+    
+    def test_kahden_eri_tuotteen_lisaaminen(self):
+        maito = Tuote('Maito',3)
+        kahvi = Tuote('Kahvi',8)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(kahvi)
+        self.assertEqual(self.kori.tavaroita_korissa(),2)
