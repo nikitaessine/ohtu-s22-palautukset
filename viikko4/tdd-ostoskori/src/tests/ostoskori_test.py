9ef3560cf562bb6f9774ddf0bbb439b35eb498b8
Nikita Essine <nikita.essine@helsinki.fi>
2022-11-22 16:05:36 +0200

    Jos korissa on kaksi samaa tuotetta ja toinen näistä poistetaan, jää koriin ostos jossa on tuotetta 1 kpl

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -99,5 +99,15 @@ class TestOstoskori(unittest.TestCase):
         self.assertEqual(ostos.tuotteen_nimi(), 'Maito')
         self.assertEqual(ostos.lukumaara(), 2)
     
+    def test_kaksi_samaa_tuotetta_josta_toinen_poistetaan(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+        self.kori.poista_tuote(maito)
+ 
+        ostos = self.kori.ostokset()[0]
+ 
+        self.assertEqual(ostos.lukumaara(), 1)
+    
 
     
