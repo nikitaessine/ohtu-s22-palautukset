a1c8b50d1251e303f73200e3e1f7eab8ad659d0d
Nikita Essine <nikita.essine@helsinki.fi>
2022-11-22 15:07:52 +0200

    tarkistetaan ettö ostoskorin hinta on oikea

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -14,3 +14,8 @@ class TestOstoskori(unittest.TestCase):
         maito = Tuote('Maito', 3)
         self.kori.lisaa_tuote(maito)
         self.assertEqual(self.kori.tavaroita_korissa(),1)
+    
+    def test_ostoskorin_hinta_oikea(self):
+        maito = Tuote('Maito', 3)
+        self.kori.lisaa_tuote(maito)
+        self.assertEqual(self.kori.hinta(), 3)
